327ea7b1cd523e8e56479cab785a551a5454bc3d
周漫 <man.man.man.man.a@gmail.com>
2021-03-31 10:54:07 +0800

    Add files via upload

---
+++ content/Q&A/BackgroundColor.m
@@ -0,0 +1,23 @@
+%% 改变背景色代码实现
+
+
+clc,clear,close all
+%% 颜色名称
+
+
+figure("Color","r");
+plot(0,0)
+%% RGB三元组
+
+
+figure("Color",[0.5,0.5,0.5]);
+plot(0,0)
+%% 十六进制颜色代码
+
+
+figure("Color","#0072BD");
+plot(0,0)
+
+% 关于颜色代码，推荐一个很好用的网站
+%% 
+% * <https://htmlcolorcodes.com/zh/ HTML颜色代码>
